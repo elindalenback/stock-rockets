4ef06024d4215a4ed34189c18f254dc31e70ca67
elindalenback <elin.dalenback@gmail.com>
2024-03-14 16:47:14 +0000

    Install summernote and add admin panel power-up

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Thread, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Thread)
+class ThreadAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'thread_text', 'topic')
+    search_fields = ['title']
+    list_filter = ('topic',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('thread_text',)
+
 
 # Register your models here.
-admin.site.register(Thread)
 admin.site.register(Comment)
