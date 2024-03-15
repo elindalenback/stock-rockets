a4c7dc587a9b5860dae598b706a1ef959da89095
elindalenback <elin.dalenback@gmail.com>
2024-03-15 22:49:41 +0000

    Add topic category to thread pages

--- forum/admin.py
+++ forum/admin.py
@@ -5,18 +5,16 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Thread)
 class ThreadAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'topic', 'created_on')
+    list_display = ('title', 'topic', 'created_on')
     search_fields = ['title']
     list_filter = ('topic',)
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('thread_text',)
 
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
 
     list_display = ('author', 'body', 'created_on', 'thread',)
     search_fields = ['body', 'author__username', ]
-    summernote_fields = ('thread_text',)
 
 
 # Register your models here.
