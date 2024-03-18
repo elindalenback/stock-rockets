530c5475518f58780e657e130a46298e1bf50214
elindalenback <elin.dalenback@gmail.com>
2024-03-18 19:08:24 +0000

    Add edit and delete button to comments

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,11 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.ThreadList.as_view(), name='home'),
     path('thread', views.create_thread, name='thread'),
     path('<slug:slug>', views.thread_detail, name='thread_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from .models import Thread, TOPICS
 from .forms import CommentForm, ThreadForm
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class ThreadList(generic.ListView):
@@ -20,7 +21,6 @@ class ThreadList(generic.ListView):
         context = super().get_context_data(**kwargs)
         context['topics'] = TOPICS
         context['thread_form'] = ThreadForm()
-        print(TOPICS)
         return context
 
 @login_required
@@ -89,3 +89,40 @@ def thread_detail(request, slug):
         "forum/thread_detail.html",
         {"thread": thread},
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Thread.objects.all()
+        thread = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.thread = thread
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('thread_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Thread.objects.all()
+    thread = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('thread_details', args=[slug]))
